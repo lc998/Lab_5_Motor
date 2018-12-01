c1cb02afc72a332d8fdb99e32aca82368b64542f
malu1798 <malu1798@student.ju.se>
2018-12-01 13:51:05 +0100

    Start Saturday

---
+++ Inc/servoMotor.h
@@ -0,0 +1,13 @@
+/*
+ * servoMotor.h
+ *
+ *  Created on: 1 dec. 2018
+ *      Author: Lucas
+ */
+
+#ifndef SERVOMOTOR_H_
+#define SERVOMOTOR_H_
+
+
+
+#endif /* SERVOMOTOR_H_ */
---
+++ Inc/stepperMotor.h
@@ -0,0 +1,13 @@
+/*
+ * stepperMotor.h
+ *
+ *  Created on: 1 dec. 2018
+ *      Author: Lucas
+ */
+
+#ifndef STEPPERMOTOR_H_
+#define STEPPERMOTOR_H_
+
+
+
+#endif /* STEPPERMOTOR_H_ */
